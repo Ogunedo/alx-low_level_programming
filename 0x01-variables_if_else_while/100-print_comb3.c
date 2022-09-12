cd4b90a43b1351ccc81d84fc33137cb91d60024f
Justine Ogunedo <justine4dtop@gmail.com>
2022-09-12 14:28:44 -0700

    Variable-if-else-while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@
  * Print only the smallest combination of two digits
  * Numbers should be printed in ascending order, with two digits
  * You can only use the putchar function
- * (every other function (printf, puts, etc…) is forbidden)*
+ * (every other function (printf, puts, etc…) is forbidden)
  * You can only use putchar five times maximum in your code
  * You are not allowed to use any variable of type char
  * All your code should be in the main function
@@ -25,22 +25,31 @@ int main(void)
 	{
 		c = 0;
 		while (c < 10)
-		{	
+		{
 			if (d != c && d < c)
 			{
 				putchar('0' + d);
 				putchar('0' + c);
-				
-				if (c + d != 17)																	{
-																								putchar(',');
-					putchar(' ');
-																							}
-																						}
 
-			c++;
-		}
-		d++;
-	}
-	putchar('\n');
-	return (0);
+				if (c + d != 17)												 
+				{																	putchar(',');														putchar(' ');
+
+																																																							}
+
+																																						}
+
+
+
+																					c++;
+
+																							}
+
+													d++;
+
+														}
+
+					putchar('\n');
+
+						return (0);
+
 }
